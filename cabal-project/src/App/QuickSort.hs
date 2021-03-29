0f42244bef457120a17eb07963d87f999754f10c
joaofsoares <joaofsoares@yahoo.com>
2021-03-29 19:01:20 +0100

    recursion

---
+++ cabal-project/src/App/QuickSort.hs
@@ -0,0 +1,8 @@
+module App.QuickSort (sort) where
+
+sort :: Ord a => [a] -> [a]
+sort [] = []
+sort (x : xs) =
+  let smallerOrEqual = [a | a <- xs, a <= x]
+      larger = [a | a <- xs, a > x]
+   in sort smallerOrEqual ++ [x] ++ sort larger
